89c21f781105aaeca962d14c1887c1c3c365ca47
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-01-04 22:06:05 +0500

    Add api key function

--- index.py
+++ index.py
@@ -3,3 +3,16 @@
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def easy_data_key_setup(api_key):
+    try:
+        if len(api_key) != 40:
+            raise ValueError("The key should be exactly 40 characters long.")
+        if not re.match("^[a-zA-Z]", api_key):
+            raise ValueError("The key should start with an alphabet character.")
+        # Set the global variable
+        global easy_data_key
+        easy_data_key = api_key
+        print("EasyData API key verified")
+    except ValueError as e:
+        print(f"Error: {e}")
